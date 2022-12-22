8088ba2cd1e21e4f82a240c9ab2aaea4a123cc3d
rrauf8178 <rrauf8178@gmail.com>
2022-12-22 18:04:08 +0400

    Creating Covid 19 whatcher using material-ui and fetching api from official website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CssBaseline } from "@mui/material";
+import { Container } from "@mui/system";
+import Grid from "@material-ui/core/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { fetchCountries } from "./api";
+import covidLogo from "./covid-19.png";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("azerbaijan");
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      const countries = await fetchCountries();
+      setCountries(countries);
+    };
+    fetchCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container>
+          <img
+            src={covidLogo}
+            alt="Covid19 Logo"
+            style={{ width: 100, height: 100, marginTop: 20 }}
+          />
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <Select
+              value={country}
+              onChange={(event) => setCountry(event.target.value)}
+            >
+              {countries.map((country) => (
+                <MenuItem value={country.Slug}>{country.Country}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
